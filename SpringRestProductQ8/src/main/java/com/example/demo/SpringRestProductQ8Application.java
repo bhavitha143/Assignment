e8f4616493035a25f5991fb153785821c70cea38
bhavitha143 <95899163+bhavitha143@users.noreply.github.com>
2022-07-06 21:38:09 +0530

    Add files via upload

---
+++ SpringRestProductQ8/src/main/java/com/example/demo/SpringRestProductQ8Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestProductQ8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestProductQ8Application.class, args);
+	}
+
+}
